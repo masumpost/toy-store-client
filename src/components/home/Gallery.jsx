d8253f54fa623865cb0b6fe271e6111e3b55c6c0
md-masum <masum.nextu@gmail.com>
2023-05-18 20:32:16 +0600

    gallery completed

---
+++ src/components/home/Gallery.jsx
@@ -0,0 +1,156 @@
+const Gallery = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-4xl text-red-600 my-8">
+        Top Sale Babies Car Toys
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 bg-blue-400 gap-8 p-5 lg:p-10 md:p-5 mx-auto justify-around">
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/VTPjvyZ/car1.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/31nfGYG/cars2.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/GFJr2yW/car3.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/ctZsMfW/cars4.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/3vWcpHs/car5.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/ctZsMfW/cars4.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/TTzdCXS/car7.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/7SK4QtN/car8.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/7SK4QtN/car8.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/7SK4QtN/car8.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/7SK4QtN/car8.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+        <div className="card card-compact w-56 md:w-60 lg:w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/st6Mcj3/car9.jpg"
+              alt="cars"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Tractor</h2>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
